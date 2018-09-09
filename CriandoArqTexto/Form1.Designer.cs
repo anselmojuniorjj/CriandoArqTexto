fee315f5ec51390a34fbd0e97ab0ecf8a26c4338
anselmojuniorjj <anselmojuniorjj@gmail.com>
2018-09-09 19:12:10 -0300

    criando arquivo de texto

---
+++ CriandoArqTexto/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace CriandoArqTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.btn_Salvar = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.btn_Exibir = new System.Windows.Forms.Button();
+            this.btn_Limpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(21, 140);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(227, 20);
+            this.txt1.TabIndex = 0;
+            // 
+            // btn_Salvar
+            // 
+            this.btn_Salvar.Location = new System.Drawing.Point(39, 181);
+            this.btn_Salvar.Name = "btn_Salvar";
+            this.btn_Salvar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Salvar.TabIndex = 1;
+            this.btn_Salvar.Text = "Salvar";
+            this.btn_Salvar.UseVisualStyleBackColor = true;
+            this.btn_Salvar.Click += new System.EventHandler(this.btn_Salvar_Click);
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(21, 12);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(227, 108);
+            this.lista.TabIndex = 3;
+            // 
+            // btn_Exibir
+            // 
+            this.btn_Exibir.Location = new System.Drawing.Point(173, 181);
+            this.btn_Exibir.Name = "btn_Exibir";
+            this.btn_Exibir.Size = new System.Drawing.Size(75, 23);
+            this.btn_Exibir.TabIndex = 4;
+            this.btn_Exibir.Text = "Exibir";
+            this.btn_Exibir.UseVisualStyleBackColor = true;
+            this.btn_Exibir.Click += new System.EventHandler(this.btn_Exibir_Click);
+            // 
+            // btn_Limpar
+            // 
+            this.btn_Limpar.Location = new System.Drawing.Point(327, 181);
+            this.btn_Limpar.Name = "btn_Limpar";
+            this.btn_Limpar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Limpar.TabIndex = 5;
+            this.btn_Limpar.Text = "Limpar";
+            this.btn_Limpar.UseVisualStyleBackColor = true;
+            this.btn_Limpar.Click += new System.EventHandler(this.btn_Limpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 227);
+            this.Controls.Add(this.btn_Limpar);
+            this.Controls.Add(this.btn_Exibir);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.btn_Salvar);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button btn_Salvar;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Button btn_Exibir;
+        private System.Windows.Forms.Button btn_Limpar;
+    }
+}
+
---
+++ CriandoArqTexto/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CriandoArqTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Salvar_Click(object sender, EventArgs e)
+        {
+            StreamWriter ficheiro = new StreamWriter(@"C:\developments\file.txt", true, Encoding.Default);
+            ficheiro.WriteLine(txt1.Text);
+            ficheiro.Dispose(); 
+
+            txt1.Text = "";
+            txt1.Focus();
+        }
+
+        private void btn_Exibir_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            StreamReader streamReader = new StreamReader(@"C:\developments\file.txt", Encoding.Default);
+
+            while(!streamReader.EndOfStream)
+                lista.Items.Add(streamReader.ReadLine());
+
+            streamReader.Dispose();
+        }
+
+        private void btn_Limpar_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+        }
+    }
+}
